b6936006e7757b83ebca7bedcfd1034b131cf9b2
xistne <du20063@gmail.com>
2024-01-22 23:36:57 +0900

    2. 퍼시스턴스레이어

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,5 +1,12 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +16,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid")
 	private String id;	// 이 오브젝트의 아이디
 	private String userId;	// 이 오브젝트를 생성한 유저의 아이디 
 	private String title;	// Todo 타이틀 예) 운동하
